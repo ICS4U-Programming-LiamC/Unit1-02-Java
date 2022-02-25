74199d66573813b1b22934a830a6537c41b255ce
Ubuntu <ubuntu@ip-172-31-94-4.ec2.internal>
2022-02-25 14:12:23 +0000

    added error checking

--- EinsteinsEquation.java
+++ EinsteinsEquation.java
@@ -7,7 +7,13 @@ class EinsteinsEquation {
     Scanner myObj = new Scanner(System.in);  // Create a Scanner object
     System.out.print("What is the mass of your object: ");
     String massString = myObj.nextLine();
-    double massDouble = Double.parseDouble(massString);
+    double massDouble = -1;
+    try {
+      massDouble = Double.parseDouble(massString);
+    } catch (NumberFormatException e) {
+      System.out.println("Not valid mass, must be a number");
+      System.exit(1);
+    }
     double energy = massDouble * (c * c);
     System.out.println(massString + "kg of matter contains " + energy + "J");
   }
